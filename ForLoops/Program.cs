dffa1fd3282f64810da0dea0510398a39cd20f60
wests <wests@C-010053>
2021-11-02 15:56:55 +0000

    Added Sum

--- ForLoops/Program.cs
+++ ForLoops/Program.cs
@@ -6,9 +6,35 @@ namespace ForLoops
     {
         static void Main(string[] args)
         {
+            double x;
+            double sum = 0;
+            double max = 0;
+            double min = 0;
+            double mean = 0;
+
+
             for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine($"{i}");
+ 
+                do
+                {
+                    Console.WriteLine($"Enter number {i}:");
+                    x = Convert.ToDouble(Console.ReadLine());
+
+                    if (x < 1 || x > 1000) //Invalid 
+                    {
+                        Console.WriteLine("Number nees to be between 1 - 1000.");
+                        i--;
+                    }
+                    else //Valid
+                    {
+                        sum = sum + x;
+                    }
+
+                } while (x < 1 && x > 1000);
+
+                //SUM, MIN, MAX, MEAN
+                Console.WriteLine($"Total is {sum}");
             }
 
         
