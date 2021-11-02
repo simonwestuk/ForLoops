86afb538a13895948dead136ae0d5a84443f1547
wests <wests@C-010053>
2021-11-02 15:29:41 +0000

    Add project files.

---
+++ ForLoops/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"{i} * {i} = {i*i}");
+            }
+
+        
+        }
+    }
+}
