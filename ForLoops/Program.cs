e01d10721996e4bd38b50cd782ad27e36448ab2b
wests <wests@C-010053>
2021-11-02 15:41:36 +0000

    Added Loop

--- ForLoops/Program.cs
+++ ForLoops/Program.cs
@@ -6,9 +6,9 @@ namespace ForLoops
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
+            for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine($"{i} * {i} = {i * i}");
+                Console.WriteLine($"{i}");
             }
 
         
